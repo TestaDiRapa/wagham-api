df88ea7b1621e016270d874eb48347d85c4689d4
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-07-21 12:09:56 +0200

    Added refresh token support

--- api/server.py
+++ api/server.py
@@ -4,7 +4,7 @@ import os
 import requests
 from flask import Flask, jsonify, make_response, send_file, request
 from flask_cors import CORS, cross_origin
-from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required, get_jwt
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 
 CONFIG = configparser.ConfigParser()
 CONFIG.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), "static/secret/config.ini"))
@@ -25,6 +25,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app)
 app.config["JWT_SECRET_KEY"] = CONFIG["JWT"]["secret"]
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = EXPIRATION
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = False
 jwt = JWTManager(app)
 
 client_id = CONFIG["DISCORD"]["client_id"]
@@ -37,19 +38,7 @@ def discord_me_request(discord_token: str) -> requests.Response:
     }
     return requests.get('https://discord.com/api/users/@me', headers=headers)
 
-def refresh_discord_token(token): 
-    payload = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'grant_type': 'refresh_token',
-        'refresh_token': token
-    }
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    return requests.post("https://discord.com/api/v10/oauth2/token", data=payload, headers=headers)
-
-def get_wagham_role(discord_token: str, discord_id: str) -> str: 
+def get_wagham_role(discord_token: str) -> str: 
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Authorization': f'Bearer {discord_token}'
@@ -67,8 +56,49 @@ def get_wagham_role(discord_token: str, discord_id: str) -> str:
         if k in member_info_response.json()["roles"]:
             return v
 
-@app.route('/user', methods=['GET'])
-def user():
+@app.route('/discord', methods=['POST'])
+def refresh_discord_token(): 
+    if request.method == 'POST':
+        post_body = request.get_json()
+        if 'token' not in post_body:
+            return make_response('Missing refresh token', 400)
+        payload = {
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'grant_type': 'refresh_token',
+            'refresh_token': post_body['token']
+        }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        token_response = requests.post('https://discord.com/api/oauth2/token', data=payload, headers=headers)
+        if token_response.status_code != 200:
+            return make_response("", token_response.status_code)
+        discord_token = token_response.json()['access_token']
+        discord_token_expiration = token_response.json()['expires_in']
+        discord_refresh_token = token_response.json()['refresh_token']
+        return jsonify(discord_token=discord_token, discord_expiration=discord_token_expiration,
+                        discord_refresh_token=discord_refresh_token)
+    else:
+        return make_response("", 405)
+
+@app.route('/refresh', methods=['POST'])
+@jwt_required(refresh=True)
+def refresh():
+    if request.method == 'POST':
+        post_body = request.get_json()
+        # if 'discordToken' not in post_body:
+        #    return make_response('Missing discord token', 400)
+        # role = get_wagham_role(post_body['discordToken'])
+        identity = get_jwt_identity()
+        access_token = create_access_token(identity=identity)
+        return jsonify(access_token=access_token, expires_in=EXPIRATION.seconds)
+    else:
+        return make_response("", 405)
+
+
+@app.route('/auth', methods=['GET'])
+def auth():
     if request.method == 'GET':
         code = request.args.get('code', default=None, type=str)
         if code is None:
@@ -84,7 +114,7 @@ def user():
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
-        discord_token_response = requests.post('https://discord.com/api/v10/oauth2/token', data=payload, headers=headers)
+        discord_token_response = requests.post('https://discord.com/api/oauth2/token', data=payload, headers=headers)
         if discord_token_response.status_code != 200:
             return make_response('Cannot obtain Discord OAuth token', discord_token_response.status_code)
         discord_token = discord_token_response.json()['access_token']
@@ -94,12 +124,14 @@ def user():
         if me_response.status_code != 200:
             return make_response('Cannot get identity', me_response.status_code)
         identity = me_response.json()['id']
-        role = get_wagham_role(discord_token, identity)
+        role = get_wagham_role(discord_token)
         access_token = create_access_token(identity=identity)
         refresh_token = create_refresh_token(identity=identity)
         return jsonify(access_token=access_token, expires_in=EXPIRATION.seconds, refresh_token=refresh_token,
                         discord_token=discord_token, discord_expiration=discord_token_expiration,
                         discord_refresh_token=discord_refresh_token, role=role)
+    else:
+        return make_response("", 405)
 
 @app.route('/content/images/<image_filename>', methods=['GET'])
 def content_image(image_filename: str):
